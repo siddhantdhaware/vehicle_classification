f740b5f6ae70bac5ea87ed71f53747a4c7e58d95
Siddhant Dhaware <sid@dhaware.com>
2022-07-31 05:16:59 +0530

    Added Zenodo and VTID2 datasets

--- split_datasets.py
+++ split_datasets.py
@@ -6,10 +6,24 @@ input_path_vehicle_type = 'datasets/data/raw/vehicle_type'
 output_path_colors_2 = 'datasets/data/split/colors_2'
 output_path_vehicle_type = 'datasets/data/split/vehicle_type'
 
-splitfolders.ratio(input_path_colors_2, output=output_path_colors_2,
-                   seed=1337, ratio=(0.7, 0.15, 0.15),
-                   group_prefix=None)
+input_path_zenodo = 'datasets/data/raw/vehicles_zenodo'
+output_path_zenodo = 'datasets/data/split/vehicles_zenodo'
 
-splitfolders.ratio(input_path_vehicle_type, output=output_path_vehicle_type,
+input_path_vtid = 'datasets/data/raw/VTID2'
+output_path_vtid = 'datasets/data/split/VTID2'
+
+# splitfolders.ratio(input_path_colors_2, output=output_path_colors_2,
+#                    seed=1337, ratio=(0.7, 0.15, 0.15),
+#                    group_prefix=None)
+
+# splitfolders.ratio(input_path_vehicle_type, output=output_path_vehicle_type,
+#                    seed=1337, ratio=(0.7, 0.15, 0.15),
+#                    group_prefix=None)
+
+# splitfolders.ratio(input_path_zenodo, output=output_path_zenodo,
+#                    seed=1337, ratio=(0.7, 0.15, 0.15),
+#                    group_prefix=None)
+
+splitfolders.ratio(input_path_vtid, output=output_path_vtid,
                    seed=1337, ratio=(0.7, 0.15, 0.15),
                    group_prefix=None)
