a60be982b50b39fd81a10e1f8156589ad2a04da1
Siddhant Dhaware <sid@dhaware.com>
2022-09-01 09:12:50 +0530

    removed some old code from helpers

--- helpers/plot_graphs.py
+++ helpers/plot_graphs.py
@@ -2,34 +2,6 @@
 
 from matplotlib import pyplot as plt
 
-# def plot_loss(history):
-#   plt.clf()
-#   loss = history.history["loss"]
-#   val_loss = history.history["val_loss"]
-#   epochs = range(1, len(loss) + 1)
-#   plt.plot(epochs, loss, "bo", label="Training loss")
-#   plt.plot(epochs, val_loss, "b", label="Validation loss")
-#   plt.title("Training and validation loss")
-#   plt.xlabel("Epochs")
-#   plt.ylabel("Loss")
-#   plt.legend()
-#   # plt.show()
-#   return plt
-
-# def plot_acc(history):
-#   plt.clf()
-#   acc = history.history["accuracy"]
-#   val_acc = history.history["val_accuracy"]
-#   epochs = range(1, len(acc) + 1)
-#   plt.plot(epochs, acc, "bo", label="Training accuracy")
-#   plt.plot(epochs, val_acc, "b", label="Validation accuracy")
-#   plt.title("Training and validation accuracy")
-#   plt.xlabel("Epochs")
-#   plt.ylabel("Accuracy")
-#   plt.legend()
-#   # plt.show()
-#   return plt
-
 # Plot the training and validation loss graph
 def plot_loss(history):
   plt.clf()
